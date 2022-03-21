6f99c811e4e5c7f31ff164a26d6d71f3fd202a0d
darkcat013 <viorel.noroc013@gmail.com>
2022-03-21 22:29:16 +0200

    start lab 4

---
+++ lab2/lab2/Program.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace lab2
+{
+    class Program
+    {
+        private static int[] _sizes = new int[] { 100, 200, 400, 800, 1600, 3200, 6400, 10000, 15000, 20000, 25000, 30000, 35000, 40000, 50000, 60000, 70000, 80000, 90000, 100000, 250000, 500000, 750000, 1000000, 2500000, 5000000, 7500000, 10000000 };
+        private static int[] RandomArr(int size, int min, int max)
+        {
+            Random rnd = new Random();
+            int[] arr = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                arr[i] = rnd.Next(min-1, max+1);
+            }
+            return arr;
+        }
+        private static void QuickSort(int[] arr, int left, int right)
+        {
+            if (left < right)
+            {
+                int pivot = Partition(arr, left, right);
+
+                if (pivot > 1)
+                {
+                    QuickSort(arr, left, pivot - 1);
+                }
+                if (pivot + 1 < right)
+                {
+                    QuickSort(arr, pivot + 1, right);
+                }
+            }
+
+        }
+        private static int Partition(int[] arr, int left, int right)
+        {
+            int pivot = arr[left];
+            while (true)
+            {
+                while (arr[left] < pivot)
+                {
+                    left++;
+                }
+                while (arr[right] > pivot)
+                {
+                    right--;
+                }
+                if (left < right)
+                {
+                    if (arr[left] == arr[right])
+                    {
+                        return right;
+                    }
+                    int temp = arr[left];
+                    arr[left] = arr[right];
+                    arr[right] = temp;
+                }
+                else
+                {
+                    return right;
+                }
+            }
+        }
+
+        static void Merge(int[] arr, int l, int m, int r)
+        {
+            int n1 = m - l + 1;
+            int n2 = r - m;
+            int[] L = new int[n1];
+            int[] R = new int[n2];
+            int i, j;
+            for (i = 0; i < n1; ++i)
+                L[i] = arr[l + i];
+            for (j = 0; j < n2; ++j)
+                R[j] = arr[m + 1 + j];
+            i = 0;
+            j = 0;
+            int k = l;
+            while (i < n1 && j < n2)
+            {
+                if (L[i] <= R[j])
+                {
+                    arr[k] = L[i];
+                    i++;
+                }
+                else
+                {
+                    arr[k] = R[j];
+                    j++;
+                }
+                k++;
+            }
+            while (i < n1)
+            {
+                arr[k] = L[i];
+                i++;
+                k++;
+            }
+            while (j < n2)
+            {
+                arr[k] = R[j];
+                j++;
+                k++;
+            }
+        }
+        static void MergeSort(int[] arr, int l, int r)
+        {
+            if (l < r)
+            {
+                int m = l + (r - l) / 2;
+                MergeSort(arr, l, m);
+                MergeSort(arr, m + 1, r);
+                Merge(arr, l, m, r);
+            }
+        }
+
+        static void HeapSort(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = n / 2 - 1; i >= 0; i--)
+                Heapify(arr, n, i);
+            for (int i = n - 1; i > 0; i--)
+            {
+                int temp = arr[0];
+                arr[0] = arr[i];
+                arr[i] = temp;
+                Heapify(arr, i, 0);
+            }
+        }
+        static void Heapify(int[] arr, int n, int i)
+        {
+            int largest = i;
+            int l = 2 * i + 1;
+            int r = 2 * i + 2;
+            if (l < n && arr[l] > arr[largest])
+                largest = l;
+            if (r < n && arr[r] > arr[largest])
+                largest = r;
+            if (largest != i)
+            {
+                int swap = arr[i];
+                arr[i] = arr[largest];
+                arr[largest] = swap;
+                Heapify(arr, n, largest);
+            }
+        }
+
+        static void CountSort(int[] arr)
+        {
+            int max = arr.Max();
+            int min = arr.Min();
+            int range = max - min + 1;
+            int[] count = new int[range];
+            int[] output = new int[arr.Length];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                count[arr[i] - min]++;
+            }
+            for (int i = 1; i < count.Length; i++)
+            {
+                count[i] += count[i - 1];
+            }
+            for (int i = arr.Length - 1; i >= 0; i--)
+            {
+                output[count[arr[i] - min] - 1] = arr[i];
+                count[arr[i] - min]--;
+            }
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = output[i];
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            foreach (int size in _sizes)
+            {
+                int[] arr = RandomArr(size, -1000000, 1000000);
+                stopwatch.Start();
+                CountSort(arr);
+                stopwatch.Stop();
+
+                Console.WriteLine($"{(stopwatch.Elapsed.Minutes * 60000) + (stopwatch.Elapsed.Seconds * 1000) + stopwatch.Elapsed.Milliseconds}");
+            }
+        }
+    }
+}
---
+++ lab3/lab3/Program.cs
@@ -0,0 +1,150 @@
+﻿using System.Diagnostics;
+
+var sizes = new int[] { 10000000, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 20000, 30000, 40000, 50000, 60000, 70000, 80000, 90000, 100000 };
+
+static bool[] Algorithm1(int n, bool[] c)
+{
+    c[1] = false;
+    int i = 2;
+    int count = 0;
+    while (i <= n)
+    {
+        if (c[i])
+        {
+            int j = 2 * i;
+            while (j <= n)
+            {
+                c[j] = false;
+                j += i;
+                count++;
+            }
+        }
+        i++;
+    }
+    return c;
+}
+
+static bool[] Algorithm2(int n, bool[] c)
+{
+    c[1] = false;
+    int i = 2;
+    int count = 0;
+    while (i <= n)
+    {
+        int j = 2 * i;
+        while (j <= n)
+        {
+            c[j] = false;
+            j += i;
+            count++;
+        }
+        i++;
+    }
+    return c;
+}
+
+static bool[] Algorithm3(int n, bool[] c)
+{
+    c[1] = false;
+    int i = 2;
+    int count = 0;
+    while (i <= n)
+    {
+        if(c[i])
+        {
+            int j = i + 1;
+            while (j <= n)
+            {
+                if(j % i ==0)
+                {
+                    c[j] = false;
+                }
+                count++;
+                j++;
+            }
+        }
+        i++;
+    }
+    Console.WriteLine(count);
+    return c;
+}
+
+static bool[] Algorithm4(int n, bool[] c)
+{
+    c[1] = false;
+    int i = 2;
+    int count = 0;
+    while (i <= n)
+    {
+        int j = 2;
+        while(j < i)
+        {
+            if(i % j == 0)
+            {
+                c[i] = false;
+            }
+            count++;
+            j++;
+        }
+        i++;
+    }
+    Console.WriteLine(count);
+    return c;
+}
+
+static bool[] Algorithm5(int n, bool[] c)
+{
+    c[1] = false;
+    int i = 2;
+    int count = 0;
+    while (i <= n)
+    {
+        int j = 2;
+        while (j <= Math.Sqrt(i))
+        {
+            if (i % j == 0)
+            {
+                c[i] = false;
+            }
+            count++;
+            j++;
+        }
+        i++;
+    }
+    Console.WriteLine(count);
+    return c;
+}
+
+
+static void PrintPrimes(bool[] c)
+{
+    for (int j = 1; j < c.Length; j++)
+    {
+        if (c[j]) Console.WriteLine(j);
+    }
+}
+
+List<int> results = new List<int>();
+List<int> timeResults = new List<int>();
+
+Stopwatch stopwatch = new Stopwatch();
+for (int i = 0; i < 1; i++)
+{
+    Console.WriteLine(sizes[i]);
+    bool[] c = Enumerable.Repeat(true, sizes[i] + 1).ToArray();
+    c[0] = false;
+    stopwatch.Start();
+    bool[] primes = Algorithm3(sizes[i], c);
+    stopwatch.Stop();
+    results.Add(primes.Count(x => x));
+    timeResults.Add((stopwatch.Elapsed.Minutes * 60000) + (stopwatch.Elapsed.Seconds * 1000) + stopwatch.Elapsed.Milliseconds);
+}
+
+foreach (var item in results)
+{
+    Console.WriteLine(item);
+}
+foreach (var item in timeResults)
+{
+    Console.WriteLine(item);
+}
